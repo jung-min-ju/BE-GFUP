d0721fc2d36ff3b3b9b7cb39bcf3775c669e4177
King's meow <mingee641@gmail.com>
2023-09-20 21:37:05 +0900

    [feat] : Service, ServiceImpl, Controller 양식 추가

---
+++ src/main/java/com/backend/GFUP/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.backend.GFUP.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/user")
+@Slf4j
+public class UserController {
+}

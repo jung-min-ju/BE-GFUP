7787bf2182e6c08de193c6aafc59e065889d66f2
King's meow <mingee641@gmail.com>
2023-09-22 17:13:13 +0900

    [feat] : 회원가입, 로그인 로직 구현

--- src/main/java/com/backend/GFUP/service/UserService.java
+++ src/main/java/com/backend/GFUP/service/UserService.java
@@ -1,4 +1,45 @@
 package com.backend.GFUP.service;
 
-public interface UserService {
+import com.backend.GFUP.dto.RegisterRequest;
+import com.backend.GFUP.dto.UserDTO;
+import com.backend.GFUP.model.User;
+import com.backend.GFUP.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    
+    @Value("${jwt.token.secret}")
+    private String secretkey;
+    private final long expireTimeMs = 1000 * 60 * 60 * 24 * 7; // 토큰 7일
+
+    public UserDTO register(RegisterRequest request) {
+        userRepository.findByEmail(request.getEmail())
+                .ifPresent(user -> {
+                    throw  new RuntimeException();
+                });
+
+        User saveUser = userRepository.save(request.toEntity(bCryptPasswordEncoder.encode(request.getPassword())));
+        return UserDTO.fromEntity(saveUser);
+    }
+
+    public String login(String email, String password) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("가입되지 않은 사용자입니다."));
+
+        if(!bCryptPasswordEncoder.matches(password, user.getPassword())) {
+            throw new RuntimeException("비밀번호가 일치하지 않습니다.");
+        }
+
+        return JwtUtil.createToken(email, expireTimeMs, secretkey);
+    }
+
+
 }

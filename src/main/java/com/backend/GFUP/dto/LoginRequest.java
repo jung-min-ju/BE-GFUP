bb3f748e831f1172d5f9a1b17851cd135139dd5f
King's meow <mingee641@gmail.com>
2023-09-22 17:10:06 +0900

    [feat] : login,register를 위한 req,res 추가

--- src/main/java/com/backend/GFUP/dto/LoginRequest.java
+++ src/main/java/com/backend/GFUP/dto/LoginRequest.java
@@ -1,11 +1,12 @@
 package com.backend.GFUP.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
-public class LoginDTO {
+@AllArgsConstructor
+public class LoginRequest {
     private String email;
     private String password;
 }
---
+++ src/main/java/com/backend/GFUP/dto/LoginResponse.java
@@ -0,0 +1,10 @@
+package com.backend.GFUP.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponse {
+    private String token;
+}

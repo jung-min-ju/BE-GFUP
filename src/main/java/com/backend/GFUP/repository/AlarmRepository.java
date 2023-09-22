7ac0e536ac821d86e835f8b79bab8f6cf34267f2
King's meow <mingee641@gmail.com>
2023-09-22 17:08:31 +0900

    [refact] : dependencies 추가 및 최적화

--- src/main/java/com/backend/GFUP/repository/AlarmRepository.java
+++ src/main/java/com/backend/GFUP/repository/AlarmRepository.java
@@ -2,6 +2,8 @@ package com.backend.GFUP.repository;
 
 import com.backend.GFUP.model.AlarmInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AlarmRepository extends JpaRepository<AlarmInfo, Long> {
 }
